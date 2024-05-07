78e6153a016de188a2f7c131fab70bee6c408006
Hiren Patel <hirenp231994@gmail.com>
2024-05-06 23:31:04 -0400

    added a back button to login in signup component

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import Login from "../Login/Login";
+
 export const SignUp = () => {
+
+    const [isLogin, SetIsLogin] = useState<boolean>(false);
+
+    const handleBackToLogin = () => {
+        SetIsLogin(true);
+    }
+
     return (
         <>
-            <h1>Sign Up Page</h1>
+            {
+                isLogin ? <Login /> :
+                <>
+                    <h1>Sign Up Page</h1>
+                    <button onClick={handleBackToLogin}>Back to Log In</button>
+                </>
+            }
         </>
     );
 }
